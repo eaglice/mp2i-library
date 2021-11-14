c3751df628c4a7e8ed9cbcecdf6a5a8222a4c1fa
ValentinMalquy <81184121+eaglice@users.noreply.github.com>
2021-11-14 21:40:12 +0100

    Add files via upload

---
+++ algo/search(2).ml
@@ -0,0 +1,34 @@
+let recherche_dicho t x = 
+    let e = Array.length t -1 in 
+    let (a,b) = ( 0,  e) in
+    let (d,f) = (ref a, ref b) in
+            while (!d < !f ) do
+                let m = (!d + !f)/2 in 
+                if t.(m) < x then
+                    d := m + 1
+                else
+                    f := m; 
+            done;
+            t.(!d) = x;;
+
+let dicho_rec t x =
+    let rec aux i j =
+    if i > j then false
+    else let m = (i + j)/2 in 
+        if t.(m) = x then true
+        else if t.(m) < x then aux (m + 1) j
+        else aux i (m- 1)
+    in aux 0 (Array.length t - 1)
+
+let tricho_rec t x =
+    let rec aux i j =
+        if i > j then false
+        else let m1 = (2*i + j + 1 )/ 3 in
+            let m2 = ( i + 2*j + 2)/3 in
+            if t.(m1) = x || t.(m2) = x then true
+            else if x < t.(m1) then aux i (m1-1)
+            else if x < t.(m2) then aux (m1 + 1) (m2 - 1)
+            else aux (m2 + 1 ) j in
+    aux 0 (Array.length t -1)
+
+
