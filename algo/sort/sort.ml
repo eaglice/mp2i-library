e8107774fce00ec8f6d0904ad453cea956f7b1f1
ValentinMalquy <81184121+eaglice@users.noreply.github.com>
2021-11-14 21:12:38 +0100

    Update sort.ml
    algorithme de tri sur les listes

--- algo/sort/sort.ml
+++ algo/sort/sort.ml
@@ -13,3 +13,34 @@ let sort_bubble t =
       if t.(j) > t.(j + 1) then swap t j (j + 1)
       done
   done;;
+(** [split l] split t in two list  *)
+let rec split = function
+    |[] -> [], []
+    |[e] -> [e], []
+    |e1::e2::q -> let q1, q2 = split q in
+    e1::q1, e2::q2;;
+  
+ (** [fusion l1 l2] merge two sorted list  *)
+ let rec fusion l1 l2 = match l1 ,l2 with
+    |[], _ -> l2
+    |_, [] -> l1
+    |e1::q1 ,e2::q2 when e1 < e2 -> e1::fusion q1 l2
+    |e1::q1, e2::q2 -> e2::fusion l1 q2;;
+
+(** [tri l] use split and fusion to sort l *)
+let rec tri = function
+    |[] -> []
+    |[e] -> [e]
+    |l -> let l1, l2 = split l in
+    fusion (tri l1) (tri l2);;
+ (** [partition l p] create two list with each element of l1 <e and each element of l2 >= 2*)   
+ let rec partition l p = match l with
+    |[] -> [],[]
+    |e::q -> let l1 ,l2 = partition q p in
+        if e < p then e::l1, l2
+        else l1, e::l2;;
+  (** [tri_rapide l] use partition to create two sorted list from l with each element of (l1) < each element of (l2) then concatenate then to  sort l *)
+  let rec tri_rapide = function
+    |[] -> []
+    | p::q -> let l1, l2 = partition q p in 
+            (tri_rapide l1)@(tri_rapide l2)
